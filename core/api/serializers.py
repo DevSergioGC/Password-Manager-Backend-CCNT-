c67141f2bafe50ca33b91d701c708aff179b6fe4
Sergio Guzmán <82333906+DnaCyphers@users.noreply.github.com>
2022-05-31 09:56:08 -0400

    Include __all__ fields of django models

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -7,23 +7,12 @@ class FolderSerializer(serializers.ModelSerializer):
     class Meta:
         
         model = Folders
-        fields = [
-            'id_folders',
-            'name',
-        ]
+        fields = "__all__"
             
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:        
         model = Items
-        fields = [
-            'id_item',
-            'name',
-            'password',
-            'description',
-            'url',
-            'folder',
-            'user',
-        ]
+        fields = "__all__"
                
 class UserSerializer(serializers.ModelSerializer):
     class Meta:        
@@ -32,17 +21,9 @@ class UserSerializer(serializers.ModelSerializer):
             'id',
             'username',
             'first_name',
-            'password',            
         ]    
         
         extra_kwargs = {'password': {
             'write_only': True,
             'required': True
-        }}
-        
-    def create(self, validated_data):
-        
-        user = User.objects.create_user(**validated_data)
-        Token.objects.create(user=user)
-        
-        return user
+        }} 
